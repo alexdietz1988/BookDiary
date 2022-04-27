bef6c458943334d85cc9a487cb53333ece1d3130
Oksanka25 <chekoksa25@gmail.com>
2022-04-27 14:06:40 -0400

    Added Schemas

--- models/Book.js
+++ models/Book.js
@@ -1,6 +1,33 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Every book must have a title"]
+    },
+    author: {
+        type: String,
+        required: [true, "Every book must have an author"]
+    },
+    yearPublished: {
+        type: Number
+    },
+    genre: {
+        type: String
+    },
+    pages: {
+        type: Number
+    },
+    audioBook: {
+        type: Boolean,
+        hours: Number
+    },
+    readingStatus: {
+        type: String
+    },
+    notes: {
+        type: String
+    }
 
 })
 
