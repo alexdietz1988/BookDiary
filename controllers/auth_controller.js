ef237d75ca8ab15377fd0f8348dc9310f68c9935
alexdietz1988 <alex.dietz@gmail.com>
2022-04-28 11:36:05 -0400

    Add register post route

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -13,6 +13,25 @@ router.get('/register', (req, res) => {
     res.render('auth/register.ejs')
 })
 
+router.post('/register', async (req, res) => {
+    try {
+        const foundUser = await User.exists({ email: req.body.email })
+        if (foundUser) return res.redirect('/login')
+
+        const salt = await bcrypt.genSalt(12)
+        const hash = await bcrypt.hash(req.body.password, salt)
+        req.body.password = hash
+
+        await User.create(req.body)
+
+        return res.redirect('/login')
+    } catch (error) {
+        console.log(error)
+        req.error = error
+        return res.send(error)
+    }
+})
+
 // Login
 router.get('/login', (req, res) => {
     res.render('auth/login.ejs')
