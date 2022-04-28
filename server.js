b31634445bfa08f6d5347dfae1752e42bd178305
alexdietz1988 <alex.dietz@gmail.com>
2022-04-28 10:55:25 -0400

    Add session configuration to server.js

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const methodOverride = require('method-override')
 require('./config/db.connection')
 app.set('view engine', 'ejs')
 
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: false }))
+
+app.use(
+    session ({
+        store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+        secret: process.env.sessionKey,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 * 2 }
+    })
+)
 
 app.use('/', controllers)
 app.use('/home', controllers)
