f44a1d8435ff1c77559ebb4e1fea3ebf2935bd01
alexdietz1988 <alex.dietz@gmail.com>
2022-04-29 09:45:44 -0400

    Remove extraneous Github messages

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     books: require('./books_controller'),
     auth: require('./auth_controller')
-=======
-    books: require('./controller')
->>>>>>> e4122b1b4a9d6b425cec3e26c973331048b1d706
 }
